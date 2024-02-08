db982a1b21f7fe98bbe561a47dc754d3aa8942cf
NikolaiToonder <nikolet@stud.ntnu.no>
2024-02-08 15:44:49 +0100

    cleaned up complex class

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,8 +2,12 @@ import vectors.Complex;
 
 public class Main {
     public static void main(String[] args) {
-      Complex c = new Complex(0.1, 0.4);
+      Complex b = new Complex(0.3, 0.6);
+      Complex a = new Complex(0.4, 0.2);
+
+      Complex c = b.subtract(a);
       System.out.println(c.sqrt().getX0());
       System.out.println(c.sqrt().getX1());
+
     }
 }
--- src/main/java/vectors/Complex.java
+++ src/main/java/vectors/Complex.java
@@ -1,21 +1,27 @@
 package vectors;
-import java.lang.Math;
 
 public class Complex extends Vector2D {
+
   /**
-   * Constructor for Complex class, takes two doubles as parameters.
-   * One for real part one for imaginary part.
+   * Constructor for Complex class, takes two doubles as parameters. One for real part one for
+   * imaginary part.
    *
-   * @param realPart x-coordinate
+   * @param realPart      x-coordinate
    * @param imaginaryPart y-coordinate
    */
   public Complex(double realPart, double imaginaryPart) {
     super(realPart, imaginaryPart);
   }
 
-  public Complex sqrt(){
-    double r = Math.sqrt(Math.sqrt(getX0()*getX0() + getX1()*getX1()));
-    double theta = Math.atan2(getX1(), getX0());
-    return new Complex(r*Math.cos(theta/2), r*Math.sin(theta/2));
+  public Complex sqrt() {
+    double x0 = getX0();
+    double x1 = getX1();
+    double sqrtr = Math.sqrt(x0 * x0 + x1 * x1);
+    return new Complex(Math.sqrt(0.5 * (sqrtr + x0)), Math.signum(x1)
+        * Math.sqrt(0.5 * (sqrtr - x0)));
+  }
+
+  public Complex subtract(Complex other) {
+    return new Complex(getX0() - other.getX0(), getX1() - other.getX1());
   }
 }
