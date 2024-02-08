c3c8a25b672724b6552db4b70fb1ceb1120e75f0
Mustafa <mustak1310@gmail.com>
2024-02-08 14:55:09 +0100

    Added logic to the multiplication method and the class constructor, added JDK documentation

--- src/main/java/Matrix2x2.java
+++ src/main/java/Matrix2x2.java
@@ -1,5 +1,7 @@
+import vectors.Vector2D;
+
 /**
- *
+ * Class representing a 2x2 matrix. Has a method to multiply a 2D vector by the matrix.
  */
 public class Matrix2x2 {
 
@@ -8,15 +10,30 @@ public class Matrix2x2 {
   private double a10;
   private double a11;
 
+
+  /**
+   * Constructor for the 2x2 matrix. The matrix is represented as follows:
+   * @param a00 The value in the first row and first column
+   * @param a01 The value in the first row and second column
+   * @param a10 The value in the second row and first column
+   * @param a11 The value in the second row and second column
+   */
   public Matrix2x2(double a00, double a01, double a10, double a11) {
-
-    multiply();
+    this.a00 = a00;
+    this.a01 = a01;
+    this.a10 = a10;
+    this.a11 = a11;
   }
 
-  private void multiply(){
-
-
+  /**
+   * Multiplies the 2x2-matrix by a 2x1 vector. The vector is brought from the Vector2D class.
+   * @param vector The 2D vector to multiply the matrix by
+   * @return The result of the multiplication
+   */
+  private Vector2D multiply(Vector2D vector){
+    return new Vector2D(a00 * vector.getX0() + a01 * vector.getX1(), a10 * vector.getX0() + a11 * vector.getX1());
   }
+
 
 
 }
