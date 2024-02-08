31707313167626e23fcd55ac722bf57333257af6
Mustafa <mustak1310@gmail.com>
2024-02-08 14:42:10 +0100

    Made Matrix2x2 class

---
+++ src/main/java/Matrix2x2.java
@@ -0,0 +1,22 @@
+/**
+ *
+ */
+public class Matrix2x2 {
+
+  private double a00;
+  private double a01;
+  private double a10;
+  private double a11;
+
+  public Matrix2x2(double a00, double a01, double a10, double a11) {
+
+    multiply();
+  }
+
+  private void multiply(){
+
+
+  }
+
+
+}
