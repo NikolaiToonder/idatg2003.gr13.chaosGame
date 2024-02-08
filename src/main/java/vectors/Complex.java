e177e50746a7eb63def8abaaeeffa6769bef343e
NikolaiToonder <nikolet@stud.ntnu.no>
2024-02-08 15:09:37 +0100

    made Complex class

---
+++ src/main/java/vectors/Complex.java
@@ -0,0 +1,19 @@
+package vectors;
+import java.lang.Math;
+
+public class Complex extends Vector2D {
+  /**
+   * Constructor for Complex class, takes two doubles as parameters.
+   * One for real part one for imaginary part.
+   *
+   * @param realPart x-coordinate
+   * @param imaginaryPart y-coordinate
+   */
+  public Complex(double realPart, double imaginaryPart) {
+    super(realPart, imaginaryPart);
+  }
+
+  public Complex sqrt(){
+
+  }
+}
