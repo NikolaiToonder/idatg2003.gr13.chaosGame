092ea0cdb2bdd09a1b9410b9d1208d54e321a9c4
NikolaiToonder <nikolet@stud.ntnu.no>
2024-02-22 14:07:10 +0100

    siste push

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,9 +2,18 @@ import javafx.application.Application;
 import transformations.JuliaTransform;
 import vectors.Complex;
 import vectors.Vector2D;
+import javafx.stage.Stage;
 
 /**
  * Main class for the program.
  */
-public class Main {
+public class Main extends Application{
+  @Override
+  public void start(Stage primaryStage) {
+    primaryStage.show();
+  }
+
+  public static void main(String[] args) {
+    launch(args);
+  }
 }
