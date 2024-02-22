18aa7a30ca75c153168ea684074c4e6e2a222c9d
NikolaiToonder <nikolet@stud.ntnu.no>
2024-02-22 13:00:33 +0100

    Gjort klar pom filen for prosjektet

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,4 @@
+import javafx.application.Application;
 import transformations.JuliaTransform;
 import vectors.Complex;
 import vectors.Vector2D;
@@ -6,13 +7,4 @@ import vectors.Vector2D;
  * Main class for the program.
  */
 public class Main {
-    public static void main(String[] args) {
-        /* Test the JuliaTransform class */
-        /* Will not be a part of the main program */
-        Complex z = new Complex(0.4, 0.2);
-        JuliaTransform transform = new JuliaTransform(new Complex(0.3, 0.6), 1);
-        Vector2D result = transform.transform(z);
-        System.out.println(result.getX0());
-        System.out.println(result.getX1());
-    }
 }
