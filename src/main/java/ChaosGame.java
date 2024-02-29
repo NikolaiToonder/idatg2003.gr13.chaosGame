165c6c8831f791cb61f87247fe9f19cc89ab962f
NikolaiToonder <nikolet@stud.ntnu.no>
2024-02-29 13:37:34 +0100

    added ChaosGameDescription, very bare bones class rn will expand later

--- src/main/java/ChaosGame.java
+++ src/main/java/ChaosGame.java
@@ -1,3 +1,11 @@
 public class ChaosGame {
+  private ChaosGameDescription description;
 
+
+  public ChaosGame(ChaosGameDescription description) {
+    setDescription(description);
+  }
+  public void setDescription(ChaosGameDescription description) {
+    this.description = description;
+  }
 }
--- src/main/java/ChaosGameDescription.java
+++ src/main/java/ChaosGameDescription.java
@@ -1,3 +1,46 @@
-public class ChaosGameDescription {
+import java.util.Iterator;
+import java.util.List;
+import transformations.Transform2D;
+import vectors.Vector2D;
 
+public class ChaosGameDescription {
+  private Vector2D minCoords;
+  private Vector2D maxCoords;
+  private List<Transform2D> transforms;
+
+  public ChaosGameDescription(List<Transform2D> transforms, Vector2D minCoords,
+      Vector2D maxCoords) {
+    setMaxCoords(maxCoords);
+    setMinCoords(minCoords);
+    setTransforms(transforms);
+  }
+
+  public Vector2D getMinCoords() {
+    return minCoords;
+  }
+
+  public Vector2D getMaxCoords() {
+    return maxCoords;
+  }
+
+  public Iterator<Transform2D> getTransforms() {
+    return transforms.iterator();
+  }
+
+  public void setMaxCoords(Vector2D maxCoords) {
+    if (maxCoords.getX0() > 0 || maxCoords.getX1() > 0) {
+      this.maxCoords = maxCoords;
+    }
+  }
+  public void setMinCoords(Vector2D minCoords) {
+    if (minCoords.getX0() > 0 || minCoords.getX1() > 0) {
+      this.minCoords = minCoords;
+    }
+  }
+  public void setTransforms(List<Transform2D> transforms) {
+    this.transforms = transforms;
+  }
+  public void addTransforms(Transform2D transform) {
+    this.transforms.add(transform);
+  }
 }
