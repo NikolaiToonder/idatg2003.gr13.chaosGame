366de11b6a64b7a1c47b56a4b61581791f0017e1
NikolaiToonder <nikolet@stud.ntnu.no>
2024-02-29 13:51:25 +0100

    made ChaosGameFileHandler

---
+++ src/main/java/ChaosGameFileHandler.java
@@ -0,0 +1,15 @@
+import java.io.BufferedWriter;
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class ChaosGameFileHandler {
+
+
+  public void writeToFile(ChaosGameDescription description, String path) {
+    File file = new File(path);
+
+  }
+
+}
