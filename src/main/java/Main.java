5eedb3491f92fa18fe5c1c8e9b8a756ab2d83f1d
NikolaiToonder <nikolet@stud.ntnu.no>
2024-02-29 13:20:21 +0100

    Added start classes for part 2 of mappe

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,19 +1,8 @@
-import javafx.application.Application;
-import transformations.JuliaTransform;
-import vectors.Complex;
-import vectors.Vector2D;
-import javafx.stage.Stage;
-
 /**
  * Main class for the program.
  */
-public class Main extends Application{
-  @Override
-  public void start(Stage primaryStage) {
-    primaryStage.show();
-  }
+public class Main {
 
   public static void main(String[] args) {
-    launch(args);
   }
 }
