994f2b2b4e144abb5f597c86b323dd301717f0c4
NikolaiToonder <nikolet@stud.ntnu.no>
2024-02-29 14:31:26 +0100

    added transformCoordsToIndicies

--- src/main/java/ChaosCanvas.java
+++ src/main/java/ChaosCanvas.java
@@ -1,4 +1,5 @@
 import javafx.scene.transform.Affine;
+import matrix.Matrix2x2;
 import transformations.AffineTransform2D;
 import vectors.Vector2D;
 
@@ -17,22 +18,34 @@ public ChaosCanvas(int width, int height, Vector2D minCoords, Vector2D maxCoords
   this.height = height;
   this.minCoords = minCoords;
   this.maxCoords = maxCoords;
+  setTransformCoordsToIndices();
 }
 
-private int getPixel(Vector2D point) {
+public int getPixel(Vector2D point) {
   return canvas[(int) point.getX0()][(int) point.getX1()];
 }
 
-private void putPixel(Vector2D point) {
+public void putPixel(Vector2D point) {
   canvas[(int) point.getX0()][(int) point.getX1()] = 1;
 }
 
-private int[][] getCanvasArray() {
+public int[][] getCanvasArray() {
   return canvas;
 }
 
-private void clear() {
+public void clear() {
   canvas = new int[width][height];
 }
 
+public void setTransformCoordsToIndices() {
+  double[][] a = {
+      {0, (width - 1) / (minCoords.getX1() - maxCoords.getX1())},
+      {(height - 1) / (maxCoords.getX0() - minCoords.getX0()), 0}
+  };
+  Vector2D b = new Vector2D((width-1)*maxCoords.getX1()/(maxCoords.getX1()-minCoords.getX1()),
+      (height-1)*minCoords.getX0()/(minCoords.getX0()-maxCoords.getX0()));
+
+  transformCoordsToIndices = new AffineTransform2D(new Matrix2x2(a[0][0], a[0][1], a[1][0], a[1][1])
+      ,b);
+}
 }
--- src/main/java/ChaosGame.java
+++ src/main/java/ChaosGame.java
@@ -1,9 +1,30 @@
+import java.util.Random;
+import transformations.AffineTransform2D;
+import vectors.Vector2D;
 public class ChaosGame {
+  private ChaosCanvas canvas;
   private ChaosGameDescription description;
-  public ChaosGame(ChaosGameDescription description) {
+  private Vector2D currentPoint;
+  private Random random;
+
+  public ChaosGame(ChaosGameDescription description, int width, int height) {
     setDescription(description);
+    setCanvas(new ChaosCanvas(width, height,
+        description.getMinCoords(), description.getMaxCoords()));
   }
   public void setDescription(ChaosGameDescription description) {
     this.description = description;
+  }
+  public void setCanvas(ChaosCanvas canvas) {
+    this.canvas = canvas;
+  }
+  public ChaosCanvas getCanvas() {
+    return this.canvas;
+  }
+  public void runSteps(int steps) {
+    for (int i = 0; i < steps; i++) {
+      this.currentPoint = description.getTransforms().next().transform(currentPoint);
+      canvas.putPixel(currentPoint);
+    }
   }
 }
