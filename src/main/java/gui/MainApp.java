d290921af7694daa884fdb66e29312d161a4ca33
Mustafa <mustak1310@gmail.com>
2024-04-30 12:03:48 +0200

    Added a minimum size the screen can be

--- src/main/java/gui/MainApp.java
+++ src/main/java/gui/MainApp.java
@@ -4,25 +4,42 @@ import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-
 /**
  * The main stage of the program. Will always be displayed, only contents change depending on what
  * class is initialized into this main stage.
  */
 public class MainApp extends Application {
 
+  private static final double MIN_WIDTH = 760; // Minimum width
+  private static final double MIN_HEIGHT = 500; // Minimum height
+
   @Override
   public void start(Stage primaryStage) {
     primaryStage.setScene(createStartupScene(primaryStage));
-    primaryStage.setHeight(750);
-    primaryStage.setWidth(760);
+    primaryStage.setMinWidth(MIN_WIDTH);
+    primaryStage.setMinHeight(MIN_HEIGHT);
     primaryStage.show();
+
+    // Add listener to stage width property
+    primaryStage.widthProperty().addListener((observable, oldValue, newValue) -> {
+      if (newValue.doubleValue() < MIN_WIDTH) {
+        primaryStage.setWidth(MIN_WIDTH);
+      }
+    });
+
+    // Add listener to stage height property
+    primaryStage.heightProperty().addListener((observable, oldValue, newValue) -> {
+      if (newValue.doubleValue() < MIN_HEIGHT) {
+        primaryStage.setHeight(MIN_HEIGHT);
+      }
+    });
   }
 
   public Scene createStartupScene(Stage primaryStage) {
     StartupPage startupPage = new StartupPage(this::changeToChaosGameView, primaryStage);
     return new Scene(startupPage.createContent());
   }
+
   public void changeToStartupScene(Stage primaryStage) {
     primaryStage.setScene(createStartupScene(primaryStage));
   }
@@ -36,6 +53,4 @@ public class MainApp extends Application {
   public static void main(String[] args) {
     launch(args);
   }
-
-
 }
